8a488c06bb683bb0077ca1ff1974d799a7b2f1e9
Niccolo <niccolonobili@gmail.com>
2021-02-23 21:59:28 +0100

    Fixed intro question

--- src/pages/components/NumericalAnalysis.js
+++ src/pages/components/NumericalAnalysis.js
@@ -2,12 +2,12 @@ const NumericalAnalysis = ({ showDesc, onToggle }) => {
     return (
         <div className="intro">
             <button type="button" className="collapsible" onClick={() => onToggle(showDesc.id)}>Numerical Analysis</button> <br/><br/>
-            {showDesc.isShown ? (<>
+            {showDesc.isShown ? (<div className="intro-text">
             <p>Numerical Analysis is the study of algorithms
             to solve problems of continuous mathematics.</p>
             <p>The types of problems studied by numerical analysis
             are continuous, thus approximations are necessary.</p>
-            </>) : (<p style={{textAlign: "center"}}>What is Numerical Analysis?</p>) } <br/>
+            </div>) : (<h1 className="intro-question" style={{textAlign: "center"}}>What is Numerical Analysis?</h1>) } <br/>
         </div>
     )
 }
